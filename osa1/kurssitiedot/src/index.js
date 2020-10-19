3d081e774bd6a6d966f231dbb9b0a316825fb90c
roosanna <roosannasiren@gmail.com>
2020-10-19 22:53:44 +0300

    first commit

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.course.name}{' '}{props.course.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => (
+    <div>
+      <Part course={props.parts[0]}/>
+      <Part course={props.parts[1]}/>
+      <Part course={props.parts[2]}/>
+    </div>
+)
+
+const Total = (props) => {
+  let total = 0
+  props.parts.forEach(course => {
+    total += course.exercises
+  })
+
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
